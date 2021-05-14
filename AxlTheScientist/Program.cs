fa4ed5f599d37221376f004407e7e5df643e5579
April Bullard <aprilnb86@gmail.com>
2021-05-14 18:22:21 -0500

    initial commit

---
+++ AxlTheScientist/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AxlTheScientist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string scientistName = "Axl";
+            int numberOfApplesSent = 1;
+            char exhibit = 'A';
+            bool appleReappeared = true;
+            double numberOfApplesReturned = 1;
+            decimal howMuchAppleMissing = 0.25m;
+
+            Console.WriteLine($"{scientistName} the scientist performed an expiriment.  He successfully teleported {numberOfApplesSent} whole apple.  It is {appleReappeared} that {numberOfApplesReturned} apple reappeared.  Yet, when the apple returned there was {howMuchAppleMissing} eaten.  He called his expiriment: exhibit {exhibit}, they do exist!");
+        }
+    }
+}
